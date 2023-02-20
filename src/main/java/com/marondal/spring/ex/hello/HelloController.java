36a1d1216633e500439ce8f6d47de86a66485e60
DESKTOP-61D76IS\user <forthewy@gmail.com>
2023-02-20 18:02:16 +0900

    [임세혁] -스프링 예제 프로젝트 최초 파일 추가

---
+++ src/main/java/com/marondal/spring/ex/hello/HelloController.java
@@ -0,0 +1,18 @@
+package com.marondal.spring.ex.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {//annotation 많이씀
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloWorld() {	
+		
+		return "Hello World!!";		
+		
+	}
+	
+}
