60e7e8d4c6246bf9feff99e3d265c2d26c3e422a
sehyeogi360 <lsh27572757@gmail.com>
2024-01-18 10:41:03 +0900

    [임세혁] -jpa 수정사항들하고 공부중..

--- src/main/java/com/marondal/spring/ex/jpa/StudentController.java
+++ src/main/java/com/marondal/spring/ex/jpa/StudentController.java
@@ -1,5 +1,6 @@
 package com.marondal.spring.ex.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,125 +10,125 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.marondal.spring.ex.jpa.domain.Student;
-//import com.marondal.spring.ex.jpa.repository.StudentRepository;
-//import com.marondal.spring.ex.jpa.service.StudentService;
+import com.marondal.spring.ex.jpa.domain.Student;
+import com.marondal.spring.ex.jpa.repository.StudentRepository;
+import com.marondal.spring.ex.jpa.service.StudentService;
 
-//@RequestMapping("/jpa/student")
-//@Controller
-//public class StudentController {
-//	
-//	@Autowired
-//	private StudentService studentService;
-//	
-//	@Autowired
-//	private StudentRepository studentRepository;
-//	@GetMapping("/create")
-//	@ResponseBody
-//	public Student createStudent() {
-//		
-//		String name = "김인규";
-//		String phoneNumber = "01012345678";
-//		String email = "lecture@hagulu.com";
-//		String dreamJob = "개발자";
-//		
-//		Student student = studentService.addStudent(name, phoneNumber, email, dreamJob);
-//		
-//		return student;
-//	}
-//	
-//	@GetMapping("/update")
-//	@ResponseBody
-//	public Student updateStudent() {
-//		// id 3인 데이터의 장래희망을 강사로 변경 
-//		Student student = studentService.updateStudentDreamJob(3, "강사");
-//		
-//		return student;
-//	}
-//	
-//	@GetMapping("/delete")
-//	@ResponseBody
-//	public String deleteStudent() {
-//		
-//		// id가 3인 행 삭제 
-//		studentService.deleteStudent(3);
-//		
-//		return "삭제 성공";
-//		
-//	}
-//	
-//	@GetMapping("/select")
-//	@ResponseBody
-//	public List<Student> selectStudent() {
-//		
-//		// 모든 행 조회 
-////		List<Student> studentList = studentRepository.findAll();	
-//		
-//		// id 기준으로 내림 차순 전체 조회 
-////		List<Student> studentList = studentRepository.findAllByOrderByIdDesc();
-//		
-//		// id 기준 내림차순 하나만 조회 
-////		List<Student> studentList = studentRepository.findTop1ByOrderByIdDesc();
-//		
-//		// 전달한 이름과 일치하는 데이터만 조회 
-////		List<Student> studentList = studentRepository.findByName("유재석");
-//		
-//		// 이름 목록과 일치하는 모든 데이터 조회 
-//		// 유재석, 조세호 
-////		List<String> nameList = new ArrayList<>();
-////		nameList.add("유재석");
-////		nameList.add("조세호");
-////		List<Student> studentList = studentRepository.findByNameIn(nameList);
-//		
-//		// 이메일에 naver가 포함된 학생 조회 
-////		List<Student> studentList = studentRepository.findByEmailContaining("naver");
-//		
-//		// 이름이 유씨인 학생 조회
-////		List<Student> studentList = studentRepository.findByNameStartingWith("조");
-//		
-//		// id 1 ~ 3 범위 조회 
-//		List<Student> studentList = studentRepository.findByIdBetweenOrderByIdDesc(1, 3);
-//		
-//		return studentList;
-//	}
-//	
-//	
-//	@ResponseBody
-//	@GetMapping("/native")
-//	public List<Student> nativeStudent() {
-//		// 장래희망이 변호사인 데이터 조회 
-//		List<Student> studentList = studentRepository.findByDreamJobNative("변호사");
-//		return studentList;
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	@GetMapping("/lombok/test")
-//	@ResponseBody
-//	public void test() {
-//		
-////		Student student = new Student();
-////		student.setName("김인규");
-////		student.setDreamJob("개발자");
-////		student.setPhoneNumber("010-1234-5678");
-//		Student student = Student.builder()
-//				.name("김인규")
-//				.dreamJob("개발자")
-//				.phoneNumber("010-1234-5678")
-//				.build();
-//		
-//		
-//		System.out.println(student);
-//		
-//		
-//		
-//	}
-//	
-//	
-//}
+@RequestMapping("/jpa/student")
+@Controller
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	@GetMapping("/create")
+	@ResponseBody
+	public Student createStudent() {
+		
+		String name = "김인규";
+		String phoneNumber = "01012345678";
+		String email = "lecture@hagulu.com";
+		String dreamJob = "개발자";
+		
+		Student student = studentService.addStudent(name, phoneNumber, email, dreamJob);
+		
+		return student;
+	}
+	
+	@GetMapping("/update")
+	@ResponseBody
+	public Student updateStudent() {
+		// id 3인 데이터의 장래희망을 강사로 변경 
+		Student student = studentService.updateStudentDreamJob(3, "강사");
+		
+		return student;
+	}
+	
+	@GetMapping("/delete")
+	@ResponseBody
+	public String deleteStudent() {
+		
+		// id가 3인 행 삭제 
+		studentService.deleteStudent(3);
+		
+		return "삭제 성공";
+		
+	}
+	
+	@GetMapping("/select")
+	@ResponseBody
+	public List<Student> selectStudent() {
+		
+		// 모든 행 조회 
+		List<Student> studentList = studentRepository.findAll();	
+		
+		// id 기준으로 내림 차순 전체 조회 
+		List<Student> studentList = studentRepository.findAllByOrderByIdDesc();
+		
+		// id 기준 내림차순 하나만 조회 
+		List<Student> studentList = studentRepository.findTop1ByOrderByIdDesc();
+		
+		// 전달한 이름과 일치하는 데이터만 조회 
+		List<Student> studentList = studentRepository.findByName("유재석");
+		
+		// 이름 목록과 일치하는 모든 데이터 조회 
+		// 유재석, 조세호 
+		List<String> nameList = new ArrayList<>();
+		nameList.add("유재석");
+		nameList.add("조세호");
+		List<Student> studentList = studentRepository.findByNameIn(nameList);
+		
+		// 이메일에 naver가 포함된 학생 조회 
+		List<Student> studentList = studentRepository.findByEmailContaining("naver");
+		
+		// 이름이 유씨인 학생 조회
+		List<Student> studentList = studentRepository.findByNameStartingWith("조");
+		
+		// id 1 ~ 3 범위 조회 
+		List<Student> studentList = studentRepository.findByIdBetweenOrderByIdDesc(1, 3);
+		
+		return studentList;
+	}
+	
+	
+	@ResponseBody
+	@GetMapping("/native")
+	public List<Student> nativeStudent() {
+		// 장래희망이 변호사인 데이터 조회 
+		List<Student> studentList = studentRepository.findByDreamJobNative("변호사");
+		return studentList;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@GetMapping("/lombok/test")
+	@ResponseBody
+	public void test() {
+		
+		Student student = new Student();
+		student.setName("김인규");
+		student.setDreamJob("개발자");
+		student.setPhoneNumber("010-1234-5678");
+		Student student = Student.builder()
+				.name("김인규")
+				.dreamJob("개발자")
+				.phoneNumber("010-1234-5678")
+				.build();
+		
+		
+		System.out.println(student);
+		
+		
+		
+	}
+	
+	
+}
--- src/main/java/com/marondal/spring/ex/jpa/repository/StudentRepository.java
+++ src/main/java/com/marondal/spring/ex/jpa/repository/StudentRepository.java
@@ -7,37 +7,39 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface StudentRepository extends JpaRepository<Student, Integer> {
-//	
-//	// id를 기준으로 내림차순으로 정렬한 모든 행 데이터 
-//		public List<Student> findAllByOrderByIdDesc();
-//		
-//		// id를 기준으로 내림차순 정렬후 하나만 조회
-//		public List<Student> findTop1ByOrderByIdDesc();
-//		
-//		// name 컬럼이 일치하는 데이터 조회 
-//		public List<Student> findByName(String name);
-//		
-//		// 전달받은 이름들과 일치하는 데이터 조회 
-//		// `name` IN ('유재석', '조세호')
-//		public List<Student> findByNameIn(List<String> nameList);
-//		
-//		// email 컬럼에 특정 문자열이 포함된 데이터 조회 
-//		// `email` LIKE '%naver%'
-//		public List<Student> findByEmailContaining(String email);
-//		
-//		// 이름이 특정 문자열로 시작되는 데이터 조회
-//		// `name` LIKE '김%'
-//		public List<Student> findByNameStartingWith(String name);
-//		
-//		// id가 특정 값들의 사이에 대응되는 데이터를 Id 내림차순으로 조회 
-//		// BETWEEN 
-//		public List<Student> findByIdBetweenOrderByIdDesc(int start, int end);
-//		
-//		// 쿼리를 통해 특정 장래 희망을 가진 데이터 조회 
-//		@Query(value="SELECT * FROM `new_student` WHERE `dreamJob` = :dreamJob", nativeQuery=true)
-//		public List<Student> findByDreamJobNative(@Param("dreamJob") String dreamJob);
-//
-//		//public Student addStudent(String name, String phoneNumber, String email, String dreamJob);
-//}
+import com.marondal.spring.ex.jpa.domain.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+	
+	// id를 기준으로 내림차순으로 정렬한 모든 행 데이터 
+		public List<Student> findAllByOrderByIdDesc();
+		
+		// id를 기준으로 내림차순 정렬후 하나만 조회
+		public List<Student> findTop1ByOrderByIdDesc();
+		
+		// name 컬럼이 일치하는 데이터 조회 
+		public List<Student> findByName(String name);
+		
+		// 전달받은 이름들과 일치하는 데이터 조회 
+		// `name` IN ('유재석', '조세호')
+		public List<Student> findByNameIn(List<String> nameList);
+		
+		// email 컬럼에 특정 문자열이 포함된 데이터 조회 
+		// `email` LIKE '%naver%'
+		public List<Student> findByEmailContaining(String email);
+		
+		// 이름이 특정 문자열로 시작되는 데이터 조회
+		// `name` LIKE '김%'
+		public List<Student> findByNameStartingWith(String name);
+		
+		// id가 특정 값들의 사이에 대응되는 데이터를 Id 내림차순으로 조회 
+		// BETWEEN 
+		public List<Student> findByIdBetweenOrderByIdDesc(int start, int end);
+		
+		// 쿼리를 통해 특정 장래 희망을 가진 데이터 조회 
+		@Query(value="SELECT * FROM `new_student` WHERE `dreamJob` = :dreamJob", nativeQuery=true)
+		public List<Student> findByDreamJobNative(@Param("dreamJob") String dreamJob);
+
+		//public Student addStudent(String name, String phoneNumber, String email, String dreamJob);
+}
